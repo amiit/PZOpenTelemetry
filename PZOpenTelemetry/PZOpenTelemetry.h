621481983dd9f8d9a67634cf0bb54a4f7d5fdf77
Amit Tiwari <amitt@zeta.tech>
2024-03-09 10:42:23 +0530

    Initial Commit

---
+++ PZOpenTelemetry/PZOpenTelemetry.h
@@ -0,0 +1,18 @@
+//
+//  PZOpenTelemetry.h
+//  PZOpenTelemetry
+//
+//  Created by AT on 09/03/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PZOpenTelemetry.
+FOUNDATION_EXPORT double PZOpenTelemetryVersionNumber;
+
+//! Project version string for PZOpenTelemetry.
+FOUNDATION_EXPORT const unsigned char PZOpenTelemetryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PZOpenTelemetry/PublicHeader.h>
+
+
